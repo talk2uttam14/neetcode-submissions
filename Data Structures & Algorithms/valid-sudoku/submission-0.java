5c3ed3e57b9aaf9c46d0f46cae3958d6e349a3a0
Talk2uttam14 <talk2uttam14@gmail.com>
2026-07-31 09:09:59 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.java
@@ -0,0 +1,49 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        for(int i = 0; i < 9; i++) {
+            Set<Character> seen = new HashSet<>();
+            for( int j = 0 ; j < 9; j++) {
+            if (board[i][j] == '.') {
+                continue;
+            }
+              if (seen.contains(board[i][j])) {
+                return false;
+              } else {
+                seen.add(board[i][j]);
+              }
+            }
+        }
+        for(int i = 0; i < 9; i++) {
+            Set<Character> seen = new HashSet<>();
+            for( int j = 0; j < 9 ; j++) {
+            if (board[j][i] == '.') {
+                continue;
+            }
+              if (seen.contains(board[j][i])) {
+                return false;
+              } else {
+                seen.add(board[j][i]);
+              }
+
+            }
+        }
+        for(int i = 0; i < 9; i += 3) {
+            for(int j = 0; j < 9; j += 3) {
+                 Set<Character> seen = new HashSet<>();
+                for(int k = i; k < i + 3; k++) {
+                    for(int l = j; l < j + 3; l++) {
+                        if (board[k][l] == '.') {
+                                continue;
+                            }
+                         if (seen.contains(board[k][l])) {
+                            return false;
+                        } else {
+                            seen.add(board[k][l]);
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
