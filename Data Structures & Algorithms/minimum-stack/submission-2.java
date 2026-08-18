4b80910160a222408adba556faa6b33cd67a0072
Talk2uttam14 <talk2uttam14@gmail.com>
2026-08-18 08:55:13 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.java
@@ -0,0 +1,33 @@
+class MinStack {
+        private Deque<Integer> stack;
+        private Deque<Integer> minStack;
+    public MinStack() {
+        stack = new ArrayDeque<>();
+        minStack = new ArrayDeque<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if(minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        } else {
+            minStack.push(minStack.peek());
+        }
+        
+    }
+    
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+        
+    }
+    
+    public int top() {
+        return stack.peek();
+        
+    }
+    
+    public int getMin() {
+       return minStack.peek();
+    }
+}
